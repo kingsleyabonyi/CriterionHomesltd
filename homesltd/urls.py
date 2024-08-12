c30d1efe08684092e5a0d6bf8772e11d4273a9dc
MogboPython <mogbo18@yahoo.com>
2024-08-12 13:37:49 +0100

    submit email, mail and contact endpoints

--- homesltd/urls.py
+++ homesltd/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserCreateView
+from .views import UserCreateView, EmailSubmitView
 
 urlpatterns = [
     path('contact_us/', UserCreateView.as_view(), name='contact'),
+    path('submit_email/', EmailSubmitView.as_view(), name='submit_email'),
 ]
